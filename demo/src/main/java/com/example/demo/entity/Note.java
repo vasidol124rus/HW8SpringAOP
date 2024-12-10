1fb3a14717f9633bce5dd78e2c99db9ae09d161c
Vasiliy Kuznetsov <vasekku25@gmail.com>
2024-12-10 22:03:45 +0700

    Note

---
+++ demo/src/main/java/com/example/demo/entity/Note.java
@@ -0,0 +1,29 @@
+package com.example.demo.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDateTime;
+
+@Data
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+//@Table(name = "notes")
+public class Note {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false)
+    private String content;
+
+    @CreationTimestamp
+    private LocalDateTime createdDate;
+}
